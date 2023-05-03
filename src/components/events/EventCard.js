8760bb0f5fc021032b5836088d1469f103ca19ba
Eric Frey <ericlfrey@gmail.com>
2023-05-03 15:15:07 -0500

    Added Game name to event card. Switching branches to do Update on game and event forms.

--- src/components/events/EventCard.js
+++ src/components/events/EventCard.js
@@ -8,6 +8,7 @@ export default function EventCard({ event }) {
           <Card.Title>{event.description}</Card.Title>
           <Card.Text>{event.date}</Card.Text>
           <Card.Text>{event.time}</Card.Text>
+          <Card.Text>Game: {event.game.title}</Card.Text>
         </Card.Body>
       </Card>
     </>
